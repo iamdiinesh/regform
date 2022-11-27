c0ea404917ac2f16dac3382160febe149141e927
Dinesh Gupta <55830977+DineshGupta-droid@users.noreply.github.com>
2022-11-27 19:42:56 +0530

    Update nextreg.php

--- nextreg.php
+++ nextreg.php
@@ -8,15 +8,10 @@
 .error {   
 color: white;  
     font-family: lato;  
-   
     display: inline-block;  
     padding: 2px 10px;  
 }  
-{  
-    padding: 0;  
-    margin: 0;  
-    box-sizing: border-box;  
-}  
+ 
 body {  
     margin: 50px auto;  
     text-align: center;  
@@ -30,30 +25,37 @@ h1 {
   text-align: center;  
   letter-spacing: 3px;  
   color: black;  
-    text-transform: uppercase;  
+  
 }  
+
+input[type=text] {    
+  border: 3px solid;    
+  border-radius: 2px;   
+  display: block;    
+  font-size:1em;    
+  font-weight: bold;    
+  margin: 1em auto;    
+  padding: 1em 4em;   
+ position: relative;
+ justify-content:left;
+   
+     
+}   
  
-input {  
-    border: 2px solid #ccc;  
-    font-size: 1.5rem;  
-    font-weight: 100;  
-    font-family: 'Lato';  
-    padding: 10px;  
-}  
 form {  
     margin: 25px auto;  
     padding: 20px;  
     border: 5px solid #ccc;  
-    width: 500px;  
-    background: #fff;  
+    width: 550px;  
+    background-color:green;  
 }  
 div.form-element {  
-    margin: 20px 0;  
+    margin: 10px 0;  
 }  
   
 input[type=submit] {    
   border: 3px solid;    
-  border-radius: 2px;      
+  border-radius: 2px;   
   display: block;    
   font-size: 1em;    
   font-weight: bold;    
@@ -83,24 +85,24 @@ function test_input($data) {
 ?>  
 <h1> Registration Form</h1>  
 <form method="POST" action="">    
-  <b> Location: </b> <input type="text" name="location">  
+  <b> Location: </b> <input type="text" name="location" placeholder="Enter your location">  
   <br> <br>  
- <b> Age: </b> <input type="text" name="myage"     >  
+ <b> Age: </b> <input type="text" name="myage"  placeholder="Enter your age"  >  
   <br> <br>  
- <b> University: </b> <input type="text" name="contact" >  
+ <b> University: </b> <input type="text" name="university" placeholder="Enter your University">  
  
   <br> <br>  
  
  
   <input type="submit" name="submit1" value="REGISTER">    
+  
 </form>  
 
 <?php
-$first_name = $_POST['name'];
+//$name= $_GET['name'];
 if ($_POST['submit1'])
-echo "Thank you for filling the form" . $first_name . "<br>";
-//echo $first_name;
-  ?>
+echo ('Thank you for filling the form '.$_GET['fname']);  
+?>
  
 </body>  
 </html>  
